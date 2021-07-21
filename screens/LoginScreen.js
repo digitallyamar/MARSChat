309846c9c6c80373f3fa1ba39e77e10b1b3ca19c
digitallyamar <digitallyamar@gmail.com>
2021-07-21 07:35:22 +0530

    Working chat app without notification support.

---
+++ screens/LoginScreen.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, KeyboardAvoidingView } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+import { auth } from "../firebase";
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error));
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        navigation.replace("Home");
+      }
+    });
+
+    // Unsubscribe if ever the component remounts
+    return unsubscribe;
+  }, []);
+
+  return (
+    <KeyboardAvoidingView behavior={null} style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri:
+            "https://static.skillshare.com/uploads/users/6709565/user-image-large.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          type="email"
+          autoFocus
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          onSubmitEditing={signIn}
+        />
+      </View>
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button
+        containerStyle={styles.button}
+        onPress={() => navigation.navigate("Register")}
+        type="outline"
+        title="Register"
+      />
+      <View style={{ height: 20 }} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
